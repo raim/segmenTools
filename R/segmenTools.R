98c2f5d0807d391fc5101c382c3e6d79c37cf192
Rainer Machne <raim@tbi.univie.ac.at>
2017-01-02 14:37:18 +0100

    adding plotdev

--- R/segmenTools.R
+++ R/segmenTools.R
@@ -64,6 +64,22 @@ ci95 <- function(x,na.rm=FALSE) {
 
 
 ### PLOT UTILS
+#' Switch between plot devices
+#' @param file.name file name without suffix (.png, etc)
+#' @param type plot type: pdf, png or eps
+#' @param width figure width in inches
+#' @param height figure height in inches
+#' @param res resolution in ppi (pixels per inch), only for 'png'
+#' @export
+plotdev <- function(file.name="test", type="png", width=5, height=5, res=100) {
+  file.name <- paste(file.name, type, sep=".")
+  if ( type == "png" )
+    grDevices::png(file.name, width=width, height=height, units="in", res=res)
+  if ( type == "eps" )
+    grDevices::postscript(file.name, width=width, height=height,paper="special")
+  if ( type == "pdf" )
+    grDevices::pdf(file.name, width=width, height=height)
+}
 
 #' Some plotting routines for overlap statistics from
 #' \code{\link{getOverlapStats}}
