4e9b053792d47fced2eff373de55e8a179696015
Rainer Machne <raim@tbi.univie.ac.at>
2017-01-20 18:56:31 +0100

    lower height in summary plot

--- scripts/grepGene.sh
+++ scripts/grepGene.sh
@@ -33,7 +33,7 @@ if  [ "$plot" = "plot" ]; then
     # GREP COORDINATES
     primsg=`echo $primseg | sed 's/^0*//g'`
     coor=`grep -P "^$primsg\t" $primsegs | cut -f 2,3,4 | sed 's/\s/,/;s/\s/:/'`
-    cmd="$GENBRO/src/plotFeature.R -i $genome --coor $coor -r $range  -s $selection -S $settings  -f png -v -o $gene"
+    cmd="$GENBRO/src/plotFeature.R -i $genome --coor $coor -r $range  -s $selection -S $settings  -f png -v -o $gene --height 4"
     echo $cmd
     $cmd 
 fi
