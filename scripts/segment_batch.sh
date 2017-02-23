b10b6207f01233a332ed74b16a6b5815417babac
raim <raim@tbi.univie.ac.at>
2017-02-23 11:50:14 +0100

    adding more required vars; shorter test

--- scripts/segment_batch.sh
+++ scripts/segment_batch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -l select=1:ncpus=1:mem=3gb
+#PBS -l select=1:ncpus=1:mem=1gb
 #PBS -l walltime=01:59:00
 #PBS -A "Coilseq"
  
@@ -10,6 +10,9 @@ module load R
 R_LIBS=/home/machne/R
 YEASTSEQ=/home/machne/data/yeast/RNAseq/results
 YEASTDAT=/home/machne/data/yeast
+GENDAT=/home/machne/data/genomeData/
+GENBRO=/home/machne/programs/genomeBrowser/
+
 chrfile=$YEASTDAT/chromosomes/sequenceIndex_R64-1-1_20110208.csv
 OUTSEQ=$YEASTSEQ
 rundate=20170223 # primseg v5 - calculated with j<=i 
@@ -17,4 +20,4 @@ rundir=$OUTSEQ/segmentation/$rundate
 
 segs=3
 
-~/programs/segmenTools/scripts/runSegmentier.R  --segs $segs -i $OUTSEQ/segmentation/primarysegments_v5/allprimseg.csv --primdir $OUTSEQ/segmentation/primarysegments_v5 --primfiles primseg_  --chrfile $chrfile --trafo ash --dc.trafo ash --dft.range 1,2,3,4,5,6,7 --K 16 --nui.thresh 0.6 --nui.cr 1:5 --scores icor --scales 1:5 --M 100,150,200 --Mn 100 --fuse.thresh 0.2 --short.name --idsuffix test --plot --fig.type pdf -o $rundir
+~/programs/segmenTools/scripts/runSegmentier.R  --segs $segs -i $OUTSEQ/segmentation/primarysegments_v5/allprimseg.csv --primdir $OUTSEQ/segmentation/primarysegments_v5 --primfiles primseg_  --chrfile $chrfile --trafo ash --dc.trafo ash --dft.range 1,2,3,4,5,6,7 --K 16 --nui.thresh 0.6 --nui.cr 1:5 --scores icor --scales 1:5 --M 100 --Mn 100 --fuse.thresh 0.2 --short.name --idsuffix test --plot --fig.type pdf -o $rundir
