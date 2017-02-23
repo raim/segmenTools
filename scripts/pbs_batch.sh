349bcbfc55a1e505fa946edda6e5a97ce68e4ce2
raim <raim@tbi.univie.ac.at>
2017-02-23 14:16:17 +0100

    script to use pbs array

---
+++ scripts/pbs_batch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#PBS -l select=1:ncpus=1:mem=500mb
+#PBS -l walltime=00:59:00
+#PBS -A "Coilseq"
+
+set -e # exit immediately upon error
+
+module load R # LOAD R
+
+# set PATH VARIABLES
+R_LIBS=/home/machne/R
+
+#### TODO: generate param list in loop (as for SGE qsub)
+#### and write to job.conf
+## numj=$(expr `cat job.conf | wc -l`)
+## qsub -o $logdir -e $logdir -t 1-${numj} scripts/pbs_batch.sh
+
+jobList="job.conf"
+job=$( head -n $PBS_ARRAYID $jobList | tail -n 1 )
+$job > $logdir/${PBS_JOBID}_${PBS_ARRAYID}.dat 2> $logdir/${PBS_JOBID}_${PBS_ARRAYID}.log
+
+
