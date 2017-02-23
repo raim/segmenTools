ad2c442d30bfa68976470a9ddb315fedc264d368
raim <raim@tbi.univie.ac.at>
2017-02-23 13:03:16 +0100

    defining log and outfiles

--- scripts/segment_batch.sh
+++ scripts/segment_batch.sh
@@ -2,7 +2,9 @@
 #PBS -l select=1:ncpus=1:mem=1gb
 #PBS -l walltime=01:59:00
 #PBS -A "Coilseq"
- 
+#PBS -e /mypath/error.txt
+#PBS -o /mypath/output.txt
+
 set -e # exit immediately upon error
 
 module load R
@@ -17,7 +19,10 @@ chrfile=$YEASTDAT/chromosomes/sequenceIndex_R64-1-1_20110208.csv
 OUTSEQ=$YEASTSEQ
 rundate=20170223 # primseg v5 - calculated with j<=i 
 rundir=$OUTSEQ/segmentation/$rundate
+logdir=$rundir/log
+mkdir -p $logdir
+
 
 segs=4441
 
-~/programs/segmenTools/scripts/runSegmentier.R  --segs $segs -i $OUTSEQ/segmentation/primarysegments_v5/allprimseg.csv --primdir $OUTSEQ/segmentation/primarysegments_v5 --primfiles primseg_  --chrfile $chrfile --trafo ash --dc.trafo ash --dft.range 1,2,3,4,5,6,7 --K 16 --nui.thresh 0.6 --nui.cr 1:5 --scores icor --scales 1:5 --M 100 --Mn 100 --fuse.thresh 0.2 --short.name --idsuffix test --plot --fig.type pdf -o $rundir --genbro $GENBRO --gendat $GENDAT
+~/programs/segmenTools/scripts/runSegmentier.R  --segs $segs -i $OUTSEQ/segmentation/primarysegments_v5/allprimseg.csv --primdir $OUTSEQ/segmentation/primarysegments_v5 --primfiles primseg_  --chrfile $chrfile --trafo ash --dc.trafo ash --dft.range 1,2,3,4,5,6,7 --K 16 --nui.thresh 0.6 --nui.cr 1:5 --scores icor --scales 1:5 --M 100 --Mn 100 --fuse.thresh 0.2 --short.name --idsuffix test --plot --fig.type pdf -o $rundir --genbro $GENBRO --gendat $GENDAT > $logdir/$PBS_JOBID".out" &> $logdir/$PBS_JOBID".log"
