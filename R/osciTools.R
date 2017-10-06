ccc4dec19763f0e873782b48e7e77de74fd16ddb
Rainer Machne <raim@tbi.univie.ac.at>
2017-10-06 09:00:12 +0200

    changed function name to non-conflciting get_fft; and amended documentation; added complex2degree as documented function

--- R/osciTools.R
+++ R/osciTools.R
@@ -1,20 +1,44 @@
 
 #' Discrete Fourier Transformation
 #' 
-#' perform Discrete Fourier Transformation using \code{mvfft} from
-#' the \code{stats} package, and returning the non-redundant (for
-#' real numbers) first half of the transform, i.e., from the DC
-#' (direct current) component to the Nyquist frequency
+#' A simple wrapper around \code{\link[stats]{mvfft}} to perform
+#' Discrete Fourier Transformation, discard redundant components
+#' (for real numbers only, the second half of the transform),
+#' and name columns by `DC` (direct current, a term from electrical
+#' engineering) and the numbers of cycles in the data set reflected
+#' by each component. The last column is the Nyquist frequency.
 #' @param x data to be transformed
 #' @export
-get.fft <- function(x) {
+get_fft <- function(x) {
     n <- floor(ncol(x)/2) +1 ## Nyquist-freq
     fft <- t(stats::mvfft(t(x)))[,1:n]
     if ( n==1 )
-        colnames(fft) <- "DC"
+        colnames(fft) <- "DC" # is this necessary or better stop()?
     else
         colnames(fft) <- c("DC",as.character(1:(n-1)))
     fft
+}
+
+#' convert complex numbers to degrees
+#'
+#' convert complex number representation of polar coordinates
+#' (such as provided by \code{\link[stats]{mvfft}} or segmenTools'
+#' wrapper \code{\link{get_fft}}
+#' to degrees, and map to 0-360 representing the first cycle
+#' in an experiment
+#' @param x a vector or matrix of complex numbers
+#' @seealso \code{\link{get_fft}}
+#' @export
+complex2degree <- function(x) {
+
+    ## atan2 gives phase shifts relative to a cosine,
+    ## from -pi (delay) to pi (advance) wrt peak;
+    ## to map phase on actual time, we need to change the sign ...
+    ph <- - atan2(Im(x),Re(x)) * 180/pi
+    ## and shift the negative phases ahead
+    ph <- ifelse(ph<=  0,ph + 360, ph)
+    ph <- ifelse(ph> 360,ph - 360, ph) # TODO: required?
+    ph
 }
 
 #' calculate peak phase
@@ -23,48 +47,51 @@ get.fft <- function(x) {
 #' at the first time point, such that phase 0/360 indicates peak
 #' at time 0, and phase 180 indicates peak at half of the cycle
 #' @param x a time-series matrix with columns as time points,
-#' or a \code{timeseries} object as returned by \code{\link{processTimeseries}}
+#' or a \code{timeseries} object as returned by
+#' \code{\link[segmenTier]{processTimeseries}}
 #' @param cycles optional integer vector for the DFT components
 #' (number of cycles in the data) for which phases are to be calculated;
 #' all are returned if \code{cycle} is not specified
 #' @param degrees logical to indicate whether phases should be reported
 #' in degrees (0 - 360) or radians (0 - 2*pi)
+#' @seealso \code{\link{get_fft}} and \code{\link{complex2degree}}
 #' @export
 calculatePhase <- function(x, cycles, degrees=TRUE) {
 
     if ( class(x)=="timeseries" ) # segmenTier class !
         dft <- x$dft
-    else dft <- get.fft(data.matrix(x))
+    else dft <- get_fft(data.matrix(x))
 
+    ## all cycles:
     if ( missing(cycles) )
         cycles <- 2:ncol(dft)
     else cycles <- cycles +1
 
+    ## get requested cycle numbers
     phase <- dft[,cycles,drop=FALSE]
-    ## TODO: check whether sign is correct?
-    ## atan2 gives phase shifts relative to a cosine, from -pi (delay)
-    ## to pi (advance) wrt peak
-    phase <- atan2(Im(phase),Re(phase)) * 180/pi
-    ## atan2: negative phase means shift to right
-    ## positive phase means shift to left
-    ## re-define to indicate peak time wrt time t0=0 to t=Period
-    ## 1) switch sign, positive means later!
-    phase <- - phase 
-    ## adjust phase angles,
-    ## such that 0 is at the beginning of data
-    phase <- ifelse(phase <=  0, phase + 360, phase)
-    phase <- ifelse(phase > 360, phase - 360, phase)
+    ## convert complex number representation to degrees
+    phase <- complex2degree(phase)
+    
+    ## convert back to radian if requested
     if ( !degrees ) phase <- phase/360 * 2*pi
+    
     phase
 }
 
 #' tests phase recovery by \code{\link{calculatePhase}}
 #'
+#' generates cosine waves with fixed periods but random
+#' amplitudes and systematic phase shifts and sampled
+#' at varying resolution to test phase recovery performance
+#' via Discrete Fourier Transformation in \code{\link{calculatePhase}}.
+#' TODO: allow random phases, noise on period and general
+#' addition of noise
 #' @param n number of cosines to generate
 #' @param cyc numbers of cycles to generate
 #' @param T period
 #' @param res resolution: samples per period
 #' @param xlim x-axis range to show, defaults to one period
+#' @seealso \code{\link{calculatePhase}}
 #' @export
 testPhase <- function(n=5, cyc=4, T=24, res=6, xlim=c(-T/res,T+T/res)) {
 
@@ -82,7 +109,7 @@ testPhase <- function(n=5, cyc=4, T=24, res=6, xlim=c(-T/res,T+T/res)) {
      
     ## phase(dat,cyc) vs. phases*180/pi
     ## TODO: why is this slightly shifted (.75 degree) wrt original phases?
-    rephases <- phase(y,cyc)[,1]
+    rephases <- calculatePhase(y,cyc)[,1]
 
     matplot(time,t(y),type="l",col=1:nrow(y),xlim=xlim,lty=1)
     abline(v=seq(0,cyc*T,T),lty=3,col="gray");abline(h=0,lty=3,col="gray") 
