4e1288908dcd0cc1847c6839e0a9f1b8c54b63a5
Rainer Machne <raim@tbi.univie.ac.at>
2017-10-09 15:55:02 +0200

    default all entries in platform table

--- R/parsers.R
+++ R/parsers.R
@@ -8,13 +8,12 @@
 #' parses GEO Soft microarray experiment archives into a
 #' data matrix and an ID-probe mapping table
 #' @param file a GEO Soft archive file (eg. GSE18902_family.soft.gz)
-#' @param idcol ID columns from the platform table; NOTE: that the first
-#' column in this vector 
+#' @param idcol columns to retrieve from the platform table
 #' @param valcol value column (currently only 1 is allowed) in the sample tables
 #' @param title if TRUE a sample title will be retrieved
 #' @param desc if TRUE, value descriptions in field "#<valcol>" will be retrieved
 #' @export
-parseGEOSoft <- function(file, idcol="ORF",valcol="VALUE", title=TRUE, desc=TRUE) {
+parseGEOSoft <- function(file, idcol, valcol="VALUE", title=TRUE, desc=TRUE) {
 
     ## working with IDs, set this to false!
     op <- options(stringsAsFactors=F)
@@ -26,6 +25,8 @@ parseGEOSoft <- function(file, idcol="ORF",valcol="VALUE", title=TRUE, desc=TRUE
     start <- grep("^!platform_table_begin",lines)
     end <- grep("^!platform_table_end",lines)
     ids <- read.delim(cnx,skip=start, nrow=end-start-2,row.names=1)
+    if ( missing(idcol) )
+        idcol <- colnames(ids)
     ids <- ids[,idcol,drop=FALSE]
     #ids[ids=="",idcol[1]] <- rownames(ids)[ids==""] 
     ## find samples
