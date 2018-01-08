6180c935d61e0ca4adfba95118d5fed259e62252
Rainer Machne <raim@tbi.univie.ac.at>
2018-01-09 00:23:56 +0100

    implemented primitive permutatin

--- scripts/segmentOverlaps.R
+++ scripts/segmentOverlaps.R
@@ -3,10 +3,12 @@
 ## SEGMENT CLASSES OVERLAP STATISTIC
 ## by permutation test analysis
 
+
+## interactive debug: command-line call with parameters
+## R --args  -q /home/raim/work/yeastSeq2016/data/segmentation/segmentTest/20170307_merge/analysis4/D.dft1-7.dcash.snr_T.raw_K.12_S.icor_E.3_M.200_nui.3/D.dft1-7.dcash.snr_T.raw_K.12_S.icor_E.3_M.200_nui.3_annotated.csv --qclass mCL --tclass mCL --antisense --chrfile /home/raim/programs/tataProject/yeast/chromosomes/sequenceIndex_R64-1-1_20110208.csv -o test.csv
+## R --args -q /home/raim/work/yeastSeq2016/data/segmentation/segmentTest/20170307_merge/analysis4/D.dft1-7.dcash.snr_T.raw_K.12_S.icor_E.3_M.200_nui.3/D.dft1-7.dcash.snr_T.raw_K.12_S.icor_E.3_M.200_nui.3_annotated.csv --qclass mCL -t /home/raim/programs/tataProject/yeast/feature_R64-1-1_20110208_withclusters.csv --tclass CL_rdx --antisense --chrfile /home/raim/programs/tataProject/yeast/chromosomes/sequenceIndex_R64-1-1_20110208.csv -o test.csv
+
 library(segmenTools)
-##segtools <- "~/programs/segmenTools/"
-##source(file.path(segtools,"R/segmenTools.R")) # for segment analysis
-##source(file.path(segtools,"R/coor2index.R")) # coor2index
 
 ## nicer timestamp
 time <- function() format(Sys.time(), "%Y%m%d %H:%M:%S")
@@ -82,6 +84,7 @@ if ( verb>0 )
 if ( verb>0 )
     msg(paste("Loading chromosome index file:", chrfile, "\t\n"))
 cf <- read.table(chrfile,sep="\t",header=FALSE)
+chrL <- cf[,3]
 chrS <- c(0,cumsum(cf[,3])) ## index of chr/pos = chrS[chr] + pos
 
 ## READ SEGMENTS TO BE TESTED 
@@ -92,11 +95,18 @@ if ( verb>0 ) msg(paste("Loading target:", target, "\t\n"))
 
 ## target = antisense of query?
 samesame <- FALSE # only do forward or reverse strand if testing against self?
+frw.str <- 1
+rev.str <- -1
 if ( target=="" & antisense ) {
     target <- query
     samesame <- TRUE
+    ## only compare forward and reverse strands for auto-target
+    query <- query[query[,"strand"]==frw.str]
+    target <- target[target[,"strand"]==rev.str,]
 } else {
     target <- read.delim(target, stringsAsFactors=FALSE)
+    ## TODO: readd ttype filter
+    target <- target[target[,"type"]=="gene",]
 }
 
 if ( verb>0 )
@@ -123,54 +133,113 @@ if ( verb>0 )
 ## for all query classes vs. all target classes
 ## allowing for sense:antisense test WITHIN same segments (query=target)
 
-## query classes
-qcls <- as.character(query[,qclass])
-qcls.srt <- unique(qcls)
-qN <- length(qcls.srt)
+getJaccard <- function(query, target, qclass, tclass) {
 
-## target classes
-tcls <- as.character(target[,tclass])
-tcls.srt <- unique(tcls)
-tN <- length(tcls.srt)
-
-## get full ranges for all query classes
-qcls.rng <- rep(list(NA), qN)
-names(qcls.rng) <- qcls.srt
-for ( i in 1:qN ) {
-    cl <- qcls.srt[i]
-    idx <- qcls==cl
-    rng <- apply(as.matrix(query[idx,c("start","end")]), 1,
-                 function(x) x["start"]:x["end"])
-    names(rng) <- NULL
-    qcls.rng[[cl]] <- unlist(rng)
-}
-
-## get full ranges for all target classes
-tcls.rng <- rep(list(NA), tN)
-names(tcls.rng) <- tcls.srt
-for ( i in 1:tN ) {
-    cl <- tcls.srt[i]
-    idx <- tcls==cl
-    rng <- apply(as.matrix(target[idx,c("start","end")]), 1,
-                 function(x) x["start"]:x["end"])
-    names(rng) <- NULL
-    tcls.rng[[cl]] <- unlist(rng)
-}
-
-## get intersect/union of all query:target class pairs
-jc <- matrix(NA, nrow=qN, ncol=tN)
-for ( i in 1:qN ) {
-    for ( j in 1:tN ) { ## asymmetric only for target = antisense of query?
-        is <- length(intersect(qcls.rng[[i]],tcls.rng[[j]]))
-        un <- length(union(qcls.rng[[i]],tcls.rng[[j]]))
-        jc[i,j] <- is/un
+    ## query classes
+    qcls <- as.factor(query[,qclass])
+    qcls.srt <- sort(unique(qcls))
+    qN <- length(qcls.srt)
+    
+    ## target classes
+    tcls <- as.factor(target[,tclass])
+    tcls.srt <- sort(unique(tcls))
+    tN <- length(tcls.srt)
+    
+    ## get full ranges for all query classes
+    qcls.rng <- rep(list(NA), qN)
+    names(qcls.rng) <- qcls.srt
+    for ( i in 1:qN ) {
+        cl <- qcls.srt[i]
+        idx <- qcls==cl
+        rng <- apply(as.matrix(query[idx,c("start","end")]), 1,
+                     function(x) x["start"]:x["end"])
+        names(rng) <- NULL
+        qcls.rng[[cl]] <- unlist(rng)
     }
+    
+    ## get full ranges for all target classes
+    tcls.rng <- rep(list(NA), tN)
+    names(tcls.rng) <- tcls.srt
+    for ( i in 1:tN ) {
+        cl <- tcls.srt[i]
+        idx <- tcls==cl
+        rng <- apply(as.matrix(target[idx,c("start","end")]), 1,
+                     function(x) x["start"]:x["end"])
+        names(rng) <- NULL
+        tcls.rng[[cl]] <- unlist(rng)
+    }
+
+    ## get intersect/union of all query:target class pairs
+    jc <- matrix(NA, nrow=qN, ncol=tN)
+    colnames(jc) <- tcls.srt
+    rownames(jc) <- qcls.srt
+    for ( i in 1:qN ) {
+        for ( j in 1:tN ) { ## asymmetric only for target = antisense of query?
+            is <- length(intersect(qcls.rng[[i]],tcls.rng[[j]]))
+            un <- length(union(qcls.rng[[i]],tcls.rng[[j]]))
+            jc[i,j] <- is/un
+        }
+    }
+    jc
 }
+
+J.real <- getJaccard(query, target, qclass, tclass)
 
 ## randomize queries
 
-## get intersegment distributions
+perm <- 100
+J.pval <- J.real
+J.pval[] <- 0
+for ( i in 1:perm ) {
 
-## TODO: same as above but after permutation of all segments
-## (or only queries)?
+    cat(paste(i/perm," "))
+    
+    ## sort
+    query <- query[order(query$start),]
+    
+    ## segment lengths
+    sglen <- apply(query[,c("start","end")], 1, diff)
+    sglen <- sglen+1
+    sgcls <- as.factor(query[,qclass])
+
+    ## inter-segment lengths
+    qnum <- nrow(query)
+    islen <- apply(cbind(query[1:(qnum-1),"end"],query[2:qnum,"start"]), 1, diff)
+    islen <- c(query[1,"start"], islen, 2*sum(chrL)-query[qnum,"end"]+1)
+    islen <- islen-1
+    
+    ## check (if start and end were added
+    if ( sum(islen)+sum(sglen) != 2*sum(chrL) )
+        stop()
+    
+    ## sample segment lengths
+    ridx <- sample(1:qnum)
+    rcls <- sgcls[ridx] # store cluster to keep cluster length dist!
+    rsglen <- sglen[ridx]
+    ## sample intersegment lengths
+    rislen <- sample(islen)
+    
+    ## construct randomized segmentation
+    tot <- length(rsglen)+length(rislen)
+    cumlen <- rep(NA,tot)
+    cumlen[seq(1,tot,2)] <- rislen
+    cumlen[seq(2,tot,2)] <- rsglen
+    cumlen <- cumsum(cumlen)
+    
+    rquery <- data.frame(start=cumlen[seq(1,tot-1,2)],
+                         end=cumlen[seq(2,tot-1,2)],
+                         type=rcls)
+    
+    ## TODO: test cluster length distribution?
+
+    J.rnd <- getJaccard(rquery, target, qclass="type", tclass)
+    J.pval <- J.pval + as.numeric(J.rnd >= J.real)
+}
+J.pval <- J.pval/perm
+
+ovl <- list()
+ovl$overlap <- round(1000*J.real)
+ovl$p.value <- J.pval
+
+plotOverlaps(ovl)
 
